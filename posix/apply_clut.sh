03dfc7a35b7afe179a83ea5a1bcae1d8a3c63ecc
Nicholas Fish <github@seaofdirac.net>
2019-12-23 22:13:34 +0100

    Apply color lookup table to an image using ImageMagick (as a POSIX shell function)

---
+++ posix/apply_clut.sh
@@ -0,0 +1,18 @@
+apply_clut ()
+{
+	command -v convert >/dev/null 2>&1 || fail "Couldn't locate convert command -- install ImageMagick"
+	command -v composite >/dev/null 2>&1 || fail "Couldn't locate composite command -- install ImageMagick"
+
+	[ -f "${1}" ] || fail "Couldn't find ${1}."
+	[ -f "${2}" ] && fail "${2} already exists."
+
+	[ ! -f /tmp/CLUT.png ] && convert -size 30x600 gradient: -rotate 90  -interpolate bilinear \( +size 'xc:#1f2c33' 'xc:#f7c3ab' 'xc:#ffffff' +append \) -clut /tmp/CLUT.png # || fail "Couldn't create CLUT."
+	convert "${1}" -modulate 100,0 /tmp/CLUT.png -clut "${2}.converted" || fail "Couldn't perform CLUT operation."
+	composite -dissolve 60 "${2}.converted" "${1}" -alpha Set "${2}.dissolved" || fail "Couldn't perform composition."
+	convert "${2}.dissolved" -normalize "${2}.normalized" || fail "Couldn't perform normalization."
+	convert "${2}.normalized" "${1}" -compose CopyOpacity -composite "${2}" || fail "Couldn't copy original alpha channel."
+
+	rm "${2}.converted" || fail "Couldn't remove ${2}.converted."
+	rm "${2}.dissolved" || fail "Couldn't remove ${2}.dissolved."
+	rm "${2}.normalized" || fail "Couldn't remove ${2}.normalized."
+}
